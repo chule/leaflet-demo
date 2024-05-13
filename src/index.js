73d60ec8fd68fe7bf2809b03452bc6432e27e17a
Igor Cuckovic <cuckovic@gmail.com>
2024-05-13 19:18:59 +0200

    height update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import "leaflet/dist/leaflet.css";
-import "leaflet/dist/leaflet.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
